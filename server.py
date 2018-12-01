f355843df343ec3b3a1f9a0864fbf8cacff6512d
David Fischer <david.fishical@gmail.com>
2018-12-01 10:41:34 +0100

    Update server.py

--- server.py
+++ server.py
@@ -1,37 +1,38 @@
 import socket
-#from hedgehog.client import connect
+from hedgehog.client import connect
 
 print("run")
 
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_socket.bind(('', 1337))
-server_socket.listen(1)
-while True:
-    (client_socket, addr) = server_socket.accept()
-    msg = client_socket.recv(1024)
-    #print(str(msg))
-    test = str(msg)
+with connect(emergency=15) as hedgehog:
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind(('', 1337))
+    server_socket.listen(1)
+    while True:
+        (client_socket, addr) = server_socket.accept()
+        msg = client_socket.recv(1024)
+        #print(str(msg))
+        test = str(msg)
 
-    isL = False
-    r = ""
-    l = ""
+        isL = False
+        r = ""
+        l = ""
 
-    i = 2
+        i = 2
 
-    while(i < len(test) - 1):
-        #print(test[i])
-        if(str(test[i]) == "."):
-            isL = True
+        while(i < len(test) - 1):
+            #print(test[i])
+            if(str(test[i]) == "."):
+                isL = True
+                i += 1
+            if(isL):
+                l += str(test[i])
+            else:
+                r += str(test[i])
             i += 1
-        if(isL):
-            l += str(test[i])
-        else:
-            r += str(test[i])
-        i += 1
-    l = int(l)
-    r = int(r)
+        l = int(l)
+        r = int(r)
 
-    print("R: " + str(r) + " L: " + str(l))
+        print("R: " + str(r) + " L: " + str(l))
 
-    #hedgehog.move(0,r)
-    #hedgehog.move(1,l)
+        hedgehog.move(0,r)
+        hedgehog.move(1,l)
