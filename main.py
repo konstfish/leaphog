feafcc905078bbba1a71c8d74c2955688582856b
David Fischer <david.fishical@gmail.com>
2018-12-01 11:20:05 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+# david fischer 2018
+# simples prog um daten vom leap zu lesen und an einen controller (hedgehog) zu senden
+
 import sys, thread, time
 import socket
 import random
@@ -5,13 +8,6 @@ import os
 import sys
 sys.path.append('llib/')
 import Leap
-
-def transmission(r, l):
-    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_addr = ('192.168.1.109', 1337)
-    client_socket.connect(server_addr)
-    client_socket.send(str(r) + "." + str(l))
-    print("sent" + str(r) + "." + str(l))
 
 class SampleListener(Leap.Listener):
     finger_names = ['Thumb', 'Index', 'Middle', 'Ring', 'Pinky']
@@ -32,52 +28,34 @@ class SampleListener(Leap.Listener):
         print "Exited"
 
     def on_frame(self, controller):
-        # Get the most recent frame and report some basic information
         frame = controller.frame()
-
-        #print "Frame id: %d, timestamp: %d, hands: %d, fingers: %d, tools: %d, gestures: %d" % (
-        #      frame.id, frame.timestamp, len(frame.hands), len(frame.fingers), len(frame.tools), len(frame.gestures()))
-
-        # Get hands
+        
         inFor = False
+        # gets hands
         for hand in frame.hands:
             inFor = True
+            # hand type
             handType = "Left" if hand.is_left else "Right"
-
-            # Get the hand's normal vector and direction
             normal = hand.palm_normal
             direction = hand.direction
-
-            # Calculate the hand's pitch, roll, and yaw angles
-            #print "  pitch: %f degrees, roll: %f degrees, yaw: %f degrees" % (
-            #     direction.pitch * Leap.RAD_TO_DEG,
-            #     normal.roll * Leap.RAD_TO_DEG,
-            #    direction.yaw * Leap.RAD_TO_DEG)
-
-
-            #print(hand.palm_position)
 
             deg = normal.roll * Leap.RAD_TO_DEG
             posi = hand.palm_position[2]
             #print(deg)
-
+            
+            #sets speeds for left and right servo
             speedl = 0
             speedr = 0
-
-            #if(posi > 70):
-            #    #print("back")
-            #    speedr += int((400 * (posi / 100)) * -1)
-            #    speedl += int((400 * (posi / 100)) * -1)
-
+            
+            
             if(posi < -70):
-                #print("vor")
                 speedr += int((400 * (posi / 100)) * -1)
                 speedl += int((400 * (posi / 100)) * -1)
 
             max = 120
             print(str(deg) + " " + handType)
-            if(deg > 25):
-                #print(handType + " Links Code " + str(deg))
+            # if hand rechts (oder links lol)
+            if(deg > 25):               
                 if deg < max:
                     per = (int((deg / max) * 1000))
                 else:
@@ -86,8 +64,8 @@ class SampleListener(Leap.Listener):
                 speedr += int(400 * (deg / 100))
                 speedl -= int(400 * (deg / 100))
 
+            # if hand rechts (oder links idk)
             if(deg < -25):
-                #print(handType + " Rechts Code " + str(deg))
                 if(deg > (max * -1)):
                     per = int((deg / (max)) * 1000)
                 else:
@@ -98,8 +76,8 @@ class SampleListener(Leap.Listener):
 
             else:
                 per = 0
-            #print(per)
-
+            
+            #fixt die speed nummern geht besser aber juckt ned
             if(speedr > 800):
                 speedr = 800
             elif(speedr < -800):
@@ -114,13 +92,8 @@ class SampleListener(Leap.Listener):
             os.system("python send.py " + str(speedr) + " " + str(speedl))
 
         if(not inFor):
+            # falls keine hand drinnen is sendet er 0 0
             os.system("python send.py 0 0")
-            #try:
-        #        transmission(i, i)
-        #        time.sleep(0.05)
-        #    except:
-        #        print("[H] Connection Lost...")
-        #        time.sleep(2)
 
     def state_string(self, state):
         if state == Leap.Gesture.STATE_START:
@@ -136,21 +109,16 @@ class SampleListener(Leap.Listener):
             return "STATE_INVALID"
 
 def main():
-    # Create a sample listener and controller
     listener = SampleListener()
     controller = Leap.Controller()
-
-    # Have the sample listener receive events from the controller
     controller.add_listener(listener)
 
-    # Keep this process running until Enter is pressed
     print "Press Enter to quit..."
     try:
         sys.stdin.readline()
     except KeyboardInterrupt:
         pass
     finally:
-        # Remove the sample listener when done
         controller.remove_listener(listener)
 
 
